97762925caffc6a4cf69087cb409c6f06f5b305a
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-30 19:29:07 -0500

    Added second solution

--- 04-WhereforeArtThou.js
+++ 04-WhereforeArtThou.js
@@ -2,15 +2,25 @@ function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
   let keys = Object.keys(source);
-  for (let i = 0; i < collection.length; i++) {
-    for (let j = 0; j < keys.length; j++) {
-      if (collection[i][keys[j]] !== source[keys[j]]) {
-        break;
-      } else if (j === keys.length - 1) {
-        arr.push(collection[i]);
-      }
-    }
-  }
+
+  // for (let i = 0; i < collection.length; i++) {
+  //   for (let j = 0; j < keys.length; j++) {
+  //     if (collection[i][keys[j]] !== source[keys[j]]) {
+  //       break;
+  //     } else if (j === keys.length - 1) {
+  //       arr.push(collection[i]);
+  //     }
+  //   }
+  // }
+
+  // OR
+
+  arr = collection.filter(item => {
+    return keys.every(key => {
+      return item[key] === source[key];
+    });
+  });
+
   // Only change code above this line
   return arr;
 }
