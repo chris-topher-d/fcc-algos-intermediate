50074dfa19fae173dacf5a68efb87f3f18f7509f
Christopher Dennis <cdennis.aus@gmail.com>
2018-10-17 21:36:40 -0500

    08-dnaPairing.js

--- 08-dnaPairing.js
+++ 08-dnaPairing.js
@@ -1,9 +1,16 @@
 function pairElement(str) {
-  let pairs = [['A', 'T'], ['T', 'A'], ['C', 'G'], ['G', 'C']];
-  return str.split('').reduce((acc, next) => {
-    pairs.forEach(pair => {
-      if (pair[0] === next) acc.push(pair);
-    });
-    return acc;
-  }, []);
+  let pairs = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C"
+  };
+
+  return str.split('').map(letter => {
+    return [letter, pairs[letter]];
+  });
 }
+
+let output = pairElement("GCG"); // [["G", "C"], ["C","G"],["G", "C"]]
+
+console.log(output);
